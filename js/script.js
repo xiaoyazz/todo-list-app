40b77b49e86f565d2768a7e133b224bb46da78ca
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2022-10-27 18:26:58 -0400

    Complete basic html, css and js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 /* To-Do List Javascript File
 ** Created by Xiaoya Zou
 ** Created on Oct 27, 2022
-*/ 
-let task = document.getElementById('task').value;
-let ctr = 0;
-let btn = document.getElementById('add');
+*/
 
-btn.addEventListener('click', () => {
-    for (let i = 0; i < ctr+1; i++){
-        localStorage.setItem('task' + i, task);
-    }
-    ctr++;
-})
+// Initialize a task array
+let taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
+let listContent = document.getElementById('todo-list-content');
+let btn = document.getElementById('add');
+btn.addEventListener('click', createTask);
+
+createTask = (e) => {
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Save input into task array
+    let taskInput = document.getElementById('task').value;
+    taskArr.push(taskInput);
+    localStorage.setItem('tasks', JSON.stringify(taskArr));
+    let len = taskArr.length;
+
+    // Get array value from localStorage and display on the index page
+    // for (let i = 0; i < taskArr.length; i++) {
+    let taskDiv = document.createElement('div');
+    let taskItem = document.createElement('p');
+    taskItem.textContent = taskArr[len - 1];
+    taskDiv.appendChild(taskItem);
+    listContent.appendChild(taskDiv);
+
+    // Add style to new task
+    taskDiv.classList.add('list-item');
+    // }
+}
