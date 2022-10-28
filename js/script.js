69f512b06f1ce5cde56461d704d348e112f7a6b8
xiaoya zou <zouxiao@sheridancollege.ca>
2022-10-28 17:04:48 -0400

    Load data automatically

--- js/script.js
+++ js/script.js
@@ -3,18 +3,31 @@
 ** Created on Oct 27, 2022
 */
 
-// Initialize a task array
-let taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
-let listContent = document.getElementById('todo-list-content');
+loadData = () => {
+    // Initialize a task array
+    let taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
+    let listContent = document.getElementById('todo-list-content');
+    let len = taskArr.length;
+    let div = document.createElement('div');
+    for (let i = 0; i < len; i++){
+        let p = document.createElement('p');
+        console.log(taskArr[i]);
+        p.innerHTML = JSON.stringify(taskArr[i]).replace(/\"/g, "");;
+        div.appendChild(p);
+        p.classList.add('list-item');
+    }
+        listContent.appendChild(div);
 
-createTask = () => {
+    // Create new task and display
+    createTask = () => {
     // Save input into task array
+    //localStorage.clear();
     let taskInput = document.getElementById('task').value;
     taskArr.push(taskInput);
     localStorage.setItem('tasks', JSON.stringify(taskArr));
     let len = taskArr.length;
 
-    // Get array value from localStorage and display on the index page
+    //Get array value from localStorage and display on the index page
 
     let taskDiv = document.createElement('div');
     let taskItem = document.createElement('p');
@@ -23,6 +36,8 @@ createTask = () => {
     listContent.appendChild(taskDiv);
 
     // Add style to new task
-    taskDiv.classList.add('list-item');
+    taskItem.classList.add('list-item');
     // }
+    }
+
 }
