8a00de3a6892570a06473fc3192bc36840f1b92a
xiaoya zou <zouxiao@sheridancollege.ca>
2022-10-28 22:35:36 -0400

    Complete basic add, complete and delete features

--- js/script.js
+++ js/script.js
@@ -9,35 +9,71 @@ loadData = () => {
     let listContent = document.getElementById('todo-list-content');
     let len = taskArr.length;
     let div = document.createElement('div');
+
+    // Display current task data from localStorage
     for (let i = 0; i < len; i++){
+        // Create new task item
         let p = document.createElement('p');
-        console.log(taskArr[i]);
+        p.classList.add('list-item');
+
+        // Create new delete button
+        let btnDelete = document.createElement('button');
+        btnDelete.innerHTML = `<i class="fa-regular fa-trash-can"></i>`;
+        btnDelete.classList.add('btn-delete');
+        btnDelete.addEventListener('click', function () {
+            taskArr.splice(i, i+1);
+            localStorage.setItem('tasks', JSON.stringify(taskArr));
+            location.reload();
+        })
+
+        // Create new complete button
+        let btnComplete = document.createElement('button');
+        btnComplete.innerHTML = `<i class="fa-regular fa-square-check"></i>`;
+        btnComplete.classList.add('btn-complete');
+        btnComplete.addEventListener('click', function () {
+            p.style.textDecoration = 'line-through 2px'; // Need to be updated. Currently the text decoration will disapear after page reload
+        })
+
+        //console.log(taskArr[i]);
         p.innerHTML = JSON.stringify(taskArr[i]).replace(/\"/g, "");;
         div.appendChild(p);
-        p.classList.add('list-item');
+        p.appendChild(btnDelete);
+        p.appendChild(btnComplete);
     }
         listContent.appendChild(div);
 
-    // Create new task and display
+    // A function to create new task and display
     createTask = () => {
-    // Save input into task array
-    //localStorage.clear();
-    let taskInput = document.getElementById('task').value;
-    taskArr.push(taskInput);
-    localStorage.setItem('tasks', JSON.stringify(taskArr));
-    let len = taskArr.length;
+        // Save input into task array
+        let taskInput = document.getElementById('task');
 
-    //Get array value from localStorage and display on the index page
+        if (taskInput.value === '') { // Prevent from entering empty task
+            alert('Please enter valid task.'); // Will update the alert with a modal
+            return false;
+        } else {
+            taskArr.push(taskInput.value);
+            localStorage.setItem('tasks', JSON.stringify(taskArr));
+            let len = taskArr.length;
 
-    let taskDiv = document.createElement('div');
-    let taskItem = document.createElement('p');
-    taskItem.textContent = taskArr[len - 1];
-    taskDiv.appendChild(taskItem);
-    listContent.appendChild(taskDiv);
+            //Get the new array element value from localStorage and display on the index page
+            let taskDiv = document.createElement('div');
+            let taskItem = document.createElement('p');
+            taskItem.textContent = taskArr[len - 1];
+            taskDiv.appendChild(taskItem);
+            listContent.appendChild(taskDiv);
 
-    // Add style to new task
-    taskItem.classList.add('list-item');
-    // }
+            // Add style to new task
+            taskItem.classList.add('list-item');
+
+            // Clear text input
+            taskInput.value = ' ';            
+        }
     }
 
+    // A function to clear the task list
+    clearList = () => {
+        localStorage.clear();
+        // Reload the page
+        location.reload();
+    }
 }
